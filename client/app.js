4ae3857d68983487fa8e9c2289da888c01cc2ded
Aaron Spafford <aaron.spafford@gmail.com>
2017-01-27 16:49:44 -0800

    play button

--- client/app.js
+++ client/app.js
@@ -1,6 +1,22 @@
 app.controller('SliderCtrl', function($scope, sliders, synthService, localData) {
 
-  var noiseArr = [
+  $scope.$on('volume changed', _.debounce(function() {
+
+    $scope.volume = localData.getVolume();
+    $scope.$apply();
+
+    var synthValue = $scope.volume * 0.01;
+
+    synthService.nodes.mixer[0].input('mixerL.mul', Number(synthValue));
+    synthService.nodes.mixer[1].input('mixerR.mul', Number(synthValue));
+
+  }, 4));
+
+});
+
+app.controller('PlayCtrl', function($scope, synthService) {
+
+  synthService.nodes.noiseArr = [
     synthService.noise(1, 2, {filterFq: 200, amFq: 0.7, phase: 1}), // mid
     synthService.noise(1, 3, {filterFq: 400, amFq: 0.5, phase: 3}),
     synthService.noise(1, 2, {filterFq: 900, amFq: 0.3, phase: 1}), // mid-high
@@ -11,94 +27,29 @@ app.controller('SliderCtrl', function($scope, sliders, synthService, localData) 
     synthService.noise(1, 3, {filterFq: 6000, amFq: 0.09, phase: 2})
   ]
 
-  var left = synthService.mixer("mixerL", 2, 0, {mul: 0.1});
-  var right = synthService.mixer("mixerR", 3, 1, {mul: 0.1});
+  synthService.nodes.mixer = [
+    synthService.mixer("mixerL", 2, 0, {mul: 0.1}),
+    synthService.mixer("mixerR", 3, 1, {mul: 0.1})
+  ]
 
   var isPlaying = false;
-
   $scope.status = 'Play';
-
   $scope.togglePlay = function() {
     isPlaying = !isPlaying;
     if (isPlaying) {
       $scope.status = 'Pause'
-      noiseArr[0].play();
+      synthService.nodes.noiseArr[0].play();
+      synthService.nodes.mixer[0].input('mixerL.mul', 0.1);
+      synthService.nodes.mixer[1].input('mixerR.mul', 0.1);
     } else {
       $scope.status = 'Play'
-      nodes.forEach(function(node, index) {
-        node.pause();
-      })
+
+      synthService.nodes.mixer[0].input('mixerL.mul', 0);
+      synthService.nodes.mixer[1].input('mixerR.mul', 0);
     }
   }
 
-  var firstTime = true;
-
-  $scope.$on('volume changed', _.debounce(function() {
-
-    $scope.volume = localData.getVolume();
-    $scope.$apply();
-
-    if (firstTime) {
-      firstTime = false;
-      $scope.togglePlay();
-    }
-
-    var synthValue = $scope.volume * 0.01;
-
-    left.input('mixerL.mul', Number(synthValue));
-    right.input('mixerR.mul', Number(synthValue));
-
-  }, 4));
-
 });
-
-// app.controller('PlayCtrl', function($scope, synthService) {
-
-//   var nodes = synthService.nodes;
-//   var numSynths = 18
-//   var bus = 0;
-//   var amFq = 1;
-//   for(var i = 1; i <= numSynths; i++) {
-
-//     var options = {
-//       mul: 0.01,
-//       phase: (5 % i),
-//       filterFq: ((numSynths + 1) - i) * 100
-//     };
-
-//     if (i < 14) {
-//       options.amFq = (1/45) * i
-//     } else {
-//       options.amFq = (1/15) * i
-//     }
-
-//     var id = "synth" + i;
-//     bus = (i % 2 === 0) ? 0 : 1;
-//     // console.log(bus,'bus');
-
-//     nodes.push(new NoiseSynth(id, bus + 2, options));
-//   }
-
-//   var isPlaying = false;
-//   $scope.status = 'Play';
-//   // $scope.togglePlay = function() {
-//   //   isPlaying = !isPlaying;
-//   //   if (isPlaying) {
-//   //     $scope.status = 'Pause'
-//   //   console.log('togglePlay1');
-//   //     nodes.forEach(function(node, index) {
-//   //       node.flock.play();
-//   //     })
-//   //   } else {
-//   //   console.log('togglePlay2');
-//   //     $scope.status = 'Play'
-//   //     nodes.forEach(function(node, index) {
-//   //       node.flock.pause();
-//   //     })
-//   //   }
-//   // }
-
-// });
 
 app.factory('sliders', function() {
   sliders = [
