dc1d7da85bc33cf6a27b2ee8c3273f729e45c63c
Aaron Spafford <aaron.spafford@gmail.com>
2017-01-28 18:51:12 -0800

    noise routine

--- client/app.js
+++ client/app.js
@@ -16,15 +16,53 @@ app.controller('SliderCtrl', function($scope, sliders, synthService, localData) 
 
 app.controller('PlayCtrl', function($scope, synthService) {
 
+  var data = {};
+  var intervalId = null;
+
+  var filterFq = 0.1;
+  function noiseRoutine() {
+    intervalId = window.setInterval(function () {
+      // get random synth
+      var synth = _.sample(synthService.nodes.noiseArr);
+      // get current am freq value
+      var value = synth.get('n1.freq');
+      // initialize min and max values for this synth
+      var rangeMax = value * 0.1; // max deviation from original value
+      if (!data[synth.id]) {
+        data[synth.id] = {};
+        data[synth.id].range = {min: value - rangeMax, max: value + rangeMax}
+        console.log(data);
+      }
+      // add or subtract to update value
+      var variance = value * 0.01; // get a small percent of current value
+      if (_.sample([0,1]) === 0) {
+        value += variance;
+      } else {
+        value -= variance;
+      }
+      // check if in range
+      if (value > data[synth.id].range.min && value < data[synth.id].range.max) {
+        console.log('updating', synth.id, value);
+        synth.input('n1.freq', Number(value));
+      } else {
+        console.log('OUT of bounds ', synth.id, value);
+      }
+    }, 500);
+  }
+
   synthService.nodes.noiseArr = [
-    synthService.noise(1, 4, {filterFq: 200, amFq: 0.7, phase: 1}), // mid
-    synthService.noise(1, 5, {filterFq: 400, amFq: 0.5, phase: 3}),
-    synthService.noise(1, 4, {filterFq: 900, amFq: 0.3, phase: 1}), // mid-high
-    synthService.noise(1, 5, {filterFq: 1700, amFq: 0.2, phase: 3}),
-    synthService.noise(1, 4, {filterFq: 60, amFq: 0.1, phase: 1}), // low
-    synthService.noise(1, 5, {filterFq: 70, amFq: 0.08, phase: 3}),
-    synthService.noise(1, 4, {filterFq: 8000, amFq: 0.2, phase: 0.5}), // high
-    synthService.noise(1, 5, {filterFq: 6000, amFq: 0.09, phase: 2})
+    synthService.noise("n1", 4, {filterFq: 200, amFq: 0.4, phase: 1}), // mid
+    synthService.noise("n1", 5, {filterFq: 400, amFq: 0.3, phase: 3}),
+    synthService.noise("n1", 4, {filterFq: 500, amFq: 0.04, phase: 1}), // mid
+    synthService.noise("n1", 5, {filterFq: 300, amFq: 0.03, phase: 3}),
+    synthService.noise("n1", 4, {filterFq: 900, amFq: 0.2, phase: 1}), // mid-high
+    synthService.noise("n1", 5, {filterFq: 1000, amFq: 0.1, phase: 3}),
+    synthService.noise("n1", 4, {filterFq: 80, amFq: 0.07, phase: 1}), // low
+    synthService.noise("n1", 5, {filterFq: 70, amFq: 0.06, phase: 3}),
+    synthService.noise("n1", 4, {filterFq: 8000, amFq: 0.05, phase: 0.5}), // high
+    synthService.noise("n1", 5, {filterFq: 6000, amFq: 0.04, phase: 2}),
+    synthService.noise("n1", 4, {filterFq: 11000, amFq: 0.05, phase: 0.5}), // high
+    synthService.noise("n1", 5, {filterFq: 9000, amFq: 0.04, phase: 2})
   ]
 
   synthService.nodes.mixer = [
@@ -40,12 +78,13 @@ app.controller('PlayCtrl', function($scope, synthService) {
     isPlaying = !isPlaying;
     if (isPlaying) {
       $scope.status = 'Pause'
+      noiseRoutine(); // start noise routine
       synthService.nodes.noiseArr[0].play();
       synthService.nodes.mixer[0].input('mixerL.mul', 0.1);
       synthService.nodes.mixer[1].input('mixerR.mul', 0.1);
     } else {
       $scope.status = 'Play'
-
+      clearInterval(intervalId); // stop noise routine
       synthService.nodes.mixer[0].input('mixerL.mul', 0);
       synthService.nodes.mixer[1].input('mixerR.mul', 0);
     }
