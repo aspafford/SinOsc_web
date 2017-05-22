4047044b6c52f2b63d208ccb708ebe69ac3cf2da
Aaron Spafford <aaron.spafford@gmail.com>
2017-05-21 17:36:11 -0700

    .

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.set('port', (process.env.PORT || 5000))
 
 app.use(express.static(__dirname + '/dist'));
 
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
