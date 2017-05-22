fc555748c582d7605a2cc633acd3e2499da87a3c
Aaron Spafford <aaron.spafford@gmail.com>
2017-05-21 20:02:45 -0700

    .

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 
-// app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 
 
 app.listen(app.get('port'), function() {
